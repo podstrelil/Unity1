e547d317abcb2e680dd4bfe1b840535edc7c14c2
karlmagnuspensa <karlmagnus.pensa@gmail.com>
2020-11-15 09:51:25 +0200

    version 1.01

---
+++ Karl Magnus/FPS/Assets/damage.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damage : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+
+            collision.gameObject.SendMessage("TakeDamage");
+        }
+
+
+
+}
+    }
---
+++ Karl Magnus/FPS/Assets/wall.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wall : MonoBehaviour
+{
+    public int health;
+    // Start is called before the first frame update
+    
+    public void TakeDamage()
+    {
+        health--;
+        if(health<= 0)
+        {
+            Destroy(gameObject);
+
+
+        }
+
+    }
+}
+
